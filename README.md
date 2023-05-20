2309c45166071ed8843a0f36ee3800d5416c65a4
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-05-20 20:25:21 -0300

    feat: :tada: Initial Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# eslint-config-leonardopn
+
+This is a ESLint config for my personal projects based on [eslint-config-rocketseat
+](https://github.com/Rocketseat/eslint-config-rocketseat)
+
+## Whats included?
+
+- Standard config base;
+- React plugin(Changed by me);
+- React Hooks plugin;
+- JSX a11y plugin;
+- Prettier(changed by me);
+
+## Setup
+
+1. Install the dependencies
+
+```
+npm i -D eslint leonardopdn/eslint-config-leonardopn
+```
+
+2. Create a `.eslintrc.json` file extending the config:
+
+```
+{
+  "extends": "leonardopdn/eslint-config-leonardopn/react"
+}
+```
+
+> You can also use a `.eslintrc.js` instead of JSON if you prefer.
---
+++ react.js
@@ -0,0 +1,55 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+    },
+    extends: ["plugin:react/recommended", "plugin:react-hooks/recommended", "standard", "plugin:prettier/recommended"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react", "jsx-a11y", "@typescript-eslint"],
+    rules: {
+        "prettier/prettier": [
+            "error",
+            {
+                printWidth: 100,
+                useTabs: true,
+                tabWidth: 4,
+                bracketSameLine: true,
+                singleQuote: false,
+                trailingComma: "es5",
+                arrowParens: "avoid",
+                endOfLine: "lf",
+            },
+        ],
+        "react/self-closing-comp": "error",
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off",
+        "jsx-a11y/alt-text": [
+            "warn",
+            {
+                elements: ["img"],
+                img: ["Image"],
+            },
+        ],
+        "jsx-a11y/aria-props": "warn",
+        "jsx-a11y/aria-proptypes": "warn",
+        "jsx-a11y/aria-unsupported-elements": "warn",
+        "jsx-a11y/role-has-required-aria-props": "warn",
+        "jsx-a11y/role-supports-aria-props": "warn",
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+        "import/parsers": {
+            [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+        },
+    },
+};
